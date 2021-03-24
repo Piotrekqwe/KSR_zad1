8b96d8b675dcd45e63b0413e4129ee04d07630f7
unknown <piotr1q2w@gmail.com>
2021-03-24 11:32:57 +0100

    javafx maven template

---
+++ src/main/java/pl/likonski/Controller.java
@@ -0,0 +1,34 @@
+package pl.likonski;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable {
+
+
+    public Button btn1, btn2;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    public void keyPress(KeyEvent key) {
+    }
+
+    public void keyRelease(KeyEvent key) {
+    }
+
+    public void reset1(ActionEvent actionEvent) {
+    }
+
+    public void reset2(ActionEvent actionEvent) {
+    }
+
+
+}
