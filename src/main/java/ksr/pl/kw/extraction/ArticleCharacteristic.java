9f161d6b038d08f52b2df7741d9dd339705d8367
Klaudia-24 <klaudia.winer@gmail.com>
2021-03-27 10:57:38 +0100

    Added features and their types for article (instead of one vector).

--- src/main/java/ksr/pl/kw/extraction/ArticleCharacteristic.java
+++ src/main/java/ksr/pl/kw/extraction/ArticleCharacteristic.java
@@ -7,9 +7,37 @@ public class ArticleCharacteristic {
     private Country country;
     private double[] characteristicVector;
 
+    private int sentencesAmount;
+    private int digitsAmount;
+    private int shortWordsAmount; //words < 4 characters
+    private int longWordsAmount; //words > 10 characters
+    private int textLength;
+    private String largestAmountCitiesCountry;
+    private String currency;//USD,EUR etc.
+    private String dateFormat;
+    private String lengthUnit;
+    private String temperatureUnit;
+
     public ArticleCharacteristic(int ID, Country country, double[] characteristicVector) {
         this.ID = ID;
         this.country = country;
         this.characteristicVector = characteristicVector;
+    }
+
+    public ArticleCharacteristic(int ID, Country country, int sentencesAmount, int digitsAmount, int shortWordsAmount,
+                                 int longWordsAmount, int textLength, String largestAmountCitiesCountry, String currency, String dateFormat,
+                                 String lengthUnit, String temperatureUnit) {
+        this.ID = ID;
+        this.country = country;
+        this.sentencesAmount = sentencesAmount;
+        this.digitsAmount = digitsAmount;
+        this.shortWordsAmount = shortWordsAmount;
+        this.longWordsAmount = longWordsAmount;
+        this.textLength = textLength;
+        this.largestAmountCitiesCountry = largestAmountCitiesCountry;
+        this.currency = currency;
+        this.dateFormat = dateFormat;
+        this.lengthUnit = lengthUnit;
+        this.temperatureUnit = temperatureUnit;
     }
 }
