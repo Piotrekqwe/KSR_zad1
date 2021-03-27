9636f0810f2d0eeedf6d772c1f2183049c969ead
Klaudia-24 <klaudia.winer@gmail.com>
2021-03-27 10:59:02 +0100

    Added features and their types for article (instead of one vector).

--- src/main/java/ksr/pl/kw/extraction/ArticleDeserializer.java
+++ src/main/java/ksr/pl/kw/extraction/ArticleDeserializer.java
@@ -1,4 +1,18 @@
 package ksr.pl.kw.extraction;
 
+import org.jsoup.nodes.Document;
+import java.util.List;
+
 public class ArticleDeserializer {
+
+    private String filePath = "K:\\PL_INFORMATYKA\\SEMESTR_6\\Komputerowe_systemy_rozpoznawania\\Projekt_1\\articles";
+    private List<Document> documents;
+    private List<ArticleDTO> trainArticles;
+    private List<ArticleDTO> testArticles;
+
+    public String getFilePath(){
+        return filePath;
+    }
+
+
 }
