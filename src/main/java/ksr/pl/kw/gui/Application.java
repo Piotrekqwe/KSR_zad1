2ba5ac6aa203d15baf49f4c6bfd5c90f6e0ecd51
Klaudia-24 <klaudia.winer@gmail.com>
2021-03-28 23:17:17 +0200

    Changed name of application.

--- src/main/java/ksr/pl/kw/gui/Application.java
+++ src/main/java/ksr/pl/kw/gui/Application.java
@@ -5,7 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ksr.pl.kw.extraction.ArticleDTO;
+import ksr.pl.kw.extraction.ArticleDeserializer;
+import ksr.pl.kw.extraction.CharacteristicsRecognitionService;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class Application extends javafx.application.Application {
@@ -14,13 +18,32 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("MainStage.fxml")));
 
-        stage.setTitle("testujemy");
+        stage.setTitle("Klasyfikacja dokumentów tekstowych");
         stage.setScene(new Scene(root,800,600));
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         launch();
+        ArticleDeserializer articleDeserializer = new ArticleDeserializer();
+        CharacteristicsRecognitionService cRS = new CharacteristicsRecognitionService();
+        for(ArticleDTO articleDTO : articleDeserializer.getAllArticles()){
+
+            //System.out.println(articleDTO.stopWordsRemover());
+
+            //System.out.println(cRS.amountOfSentences(articleDTO));
+            //System.out.println(cRS.digitsAmount(articleDTO));
+            //System.out.println(cRS.shortWordsAmount(articleDTO));
+            //System.out.println(cRS.longWordsAmount(articleDTO));
+            //System.out.println(cRS.wordsAmountInArticle(articleDTO));
+            //System.out.println(cRS.largestAmountCitiesCountry(articleDTO));
+            //System.out.println(cRS.largestAmountCurrency(articleDTO));
+            //System.out.println(cRS.largestAmountDateFormat(articleDTO));
+            //System.out.println(cRS.largestAmountLengthUnit(articleDTO));
+            //System.out.println(cRS.largestAmountTemperatureUnit(articleDTO));
+
+        }
+
     }
 
 }
